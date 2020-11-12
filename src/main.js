4f6802f05a175441bcd3e8ca61dc659cf5aadbb2
Joe Webster <joseph.scott.webster@gmail.com>
2020-11-12 17:02:08 -0500

    Create createIdea function

--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
+let cardForm = document.querySelector(".js-card-form");
+let cardTitle = document.querySelector(".js-card-form-title");
+let cardBody = document.querySelector(".js-card-form-body");
 let saveButton = document.querySelector('.js-save-button');
 
-saveButton.addEventListener('click', createIdea);
+let ideas = [];
 
 function createIdea(e) {
   e.preventDefault();
+  let title = cardTitle.value;
+  let body = cardBody.value;
+  let idea = new Idea(title, body);
 
-  console.log(e.target)
+  cardForm.reset();
+
+  console.log(idea);
 }
+  
+saveButton.addEventListener('click', createIdea);
