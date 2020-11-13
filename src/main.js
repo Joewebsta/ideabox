c3c64f169807f6e9f1e8a4c608ad25cf4dc20d9e
Joe Webster <joseph.scott.webster@gmail.com>
2020-11-13 17:32:56 -0500

    Append new card to cards section

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ const cardForm = document.querySelector(".js-card-form");
 const cardTitle = document.querySelector(".js-card-form-title");
 const cardBody = document.querySelector(".js-card-form-body");
 const saveButton = document.querySelector('.js-save-button');
-
+const cards = document.querySelector('.js-cards');
 
 let ideas = [];
 
@@ -18,8 +18,6 @@ function saveIdea(e) {
 
   idea.saveToStorage();
   ideas.push(idea);
-
-  cardForm.reset();
 }
 
 function deleteIdea(id) {
@@ -27,6 +25,37 @@ function deleteIdea(id) {
  const ideaIdx = ideas.findIndex(idea => idea.id === id);
  ideas.splice(ideaIdx, 1);
  idea.deleteFromStorage();
+}
+
+function displayIdea() {
+  const title = cardTitle.value;
+  const body = cardBody.value;
+  const ideaHTML = createCardHTML(title, body);
+  cards.insertAdjacentHTML('beforeend', ideaHTML);
+}
+
+function createCardHTML(title, body) {
+  return `<div class="card">
+    <header class="card-header">
+      <img src="./assets/star.svg" alt="Star icon">
+      <img src="./assets/delete.svg" alt="Delete icon">
+    </header>
+    <section class="card-body">
+      <h2>${title}</h2>
+      <p>${body}</p>
+    </section>
+    <footer class="card-footer">
+      <img src="assets/comment.svg" alt="Comment icon">
+      <p>Comment</p>
+    </footer>
+  </div>
+  `
+}
+
+function handleClick(e) {
+  saveIdea(e);
+  displayIdea();
+  cardForm.reset();
 }
 
 function monitorCardFields() {
@@ -37,5 +66,5 @@ function monitorCardFields() {
   }
 }
 
-saveButton.addEventListener('click', saveIdea);
+saveButton.addEventListener('click', handleClick);
 cardForm.addEventListener('keyup', monitorCardFields);
